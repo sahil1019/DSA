3adac554c49a0438c65a4b9eeec91a6089606b80
sahil1019 <114853479+sahil1019@users.noreply.github.com>
2026-06-11 10:10:07 +0530

    Time: 1747 ms (5.05%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -3,6 +3,7 @@ SELECT
     p.product_name,
     s.year,
     s.price
-FROM Sales s
-JOIN Product p
-    ON s.product_id = p.product_id;
+FROM Product p
+INNER JOIN Sales AS s
+
+ON s.product_id = p.product_id;
