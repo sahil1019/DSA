d7ace0e4ddb585e7c6f26d2d8151dd46a9e4bf07
sahil1019 <114853479+sahil1019@users.noreply.github.com>
2026-06-12 14:44:42 +0530

    Time: 0 ms (100%), Space: 19.4 MB (8.28%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        states = minutesToTest // minutesToDie + 1
+        pigs = 0
+        
+        while states ** pigs < buckets:
+            pigs += 1
+        
+        return pigs
