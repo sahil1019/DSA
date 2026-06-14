56b7d4632593bb0ab69c9a2f54ae0de00bbc39ef
sahil1019 <114853479+sahil1019@users.noreply.github.com>
2026-06-14 22:43:13 +0530

    Time: 57 ms (42.77%), Space: 21 MB (20.93%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,15 @@
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+
+            for child in node.children:
+                dfs(child)
+
+            res.append(node.val)
+
+        dfs(root)
+        return res
