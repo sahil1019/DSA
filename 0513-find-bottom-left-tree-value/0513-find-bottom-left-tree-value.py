5b0ca8ebd38563c757967eea39c6ed87d3d01c6d
sahil1019 <114853479+sahil1019@users.noreply.github.com>
2026-06-14 21:58:22 +0530

    Time: 1 ms (62.2%), Space: 22.4 MB (65.89%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+
+        return node.val
