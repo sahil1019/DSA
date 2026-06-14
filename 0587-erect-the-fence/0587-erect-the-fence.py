e82428f1db4f3953b7ad9c6feeaf25505fab6d39
sahil1019 <114853479+sahil1019@users.noreply.github.com>
2026-06-14 22:42:00 +0530

    Time: 24 ms (82.63%), Space: 19.7 MB (69.48%) - LeetHub

---
+++ 0587-erect-the-fence/0587-erect-the-fence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def outerTrees(self, trees: List[List[int]]) -> List[List[int]]:
+        trees.sort()
+
+        def cross(o, a, b):
+            return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+        lower = []
+        for p in trees:
+            while len(lower) >= 2 and cross(lower[-2], lower[-1], p) < 0:
+                lower.pop()
+            lower.append(tuple(p))
+
+        upper = []
+        for p in reversed(trees):
+            while len(upper) >= 2 and cross(upper[-2], upper[-1], p) < 0:
+                upper.pop()
+            upper.append(tuple(p))
+
+        return list(map(list, set(lower + upper)))
